aff02fbf323b557e41f5c3cf9cd514a4c3eed3a7
Berk <70212751+BerkTas20@users.noreply.github.com>
2021-03-21 00:51:16 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new CustomerCheckManager());
+            customerManager.Save(new Customer
+            {
+                DateOfBirth = new DateTime(1999,1,9),
+                FirstName = "Berk",
+                LastName = "Tas",
+                IdentifyNumber = "12345678912",
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
+
+   
